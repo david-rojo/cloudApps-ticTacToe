1294cff92ce3a61c2cb89d5344ca46c532294f30
david-rojo <davidrojotech@gmail.com>
2020-11-28 22:29:32 +0100

    refactor, delete not needed attributes in State

--- src/main/java/cloudapps/tictactoe/models/State.java
+++ src/main/java/cloudapps/tictactoe/models/State.java
@@ -4,17 +4,11 @@ public class State {
 	
 	private StateValue stateValue;
 	
-	private String[] possibleValues = new String[] {"INITIAL", "IN_GAME", "RESUME", "EXIT"};
-	
-	private int index;
-	
 	public State() {
-		index = 0;
 		stateValue = StateValue.INITIAL;
 	}
 	
 	public void next() {
-		index++;
 		stateValue = StateValue.values()[this.stateValue.ordinal() + 1];
 	}
 	
