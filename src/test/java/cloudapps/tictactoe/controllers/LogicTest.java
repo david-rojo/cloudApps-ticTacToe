fa73136474c6a4b5c24705467d6384d992d43637
david-rojo <davidrojotech@gmail.com>
2020-11-28 22:47:27 +0100

    created LogicTest with empty test

---
+++ src/test/java/cloudapps/tictactoe/controllers/LogicTest.java
@@ -0,0 +1,12 @@
+package cloudapps.tictactoe.controllers;
+
+import org.junit.jupiter.api.Test;
+
+class LogicTest {
+	
+	@Test
+	void testGivenNewLogicWhenGetControllerThenController() {
+		
+	}
+
+}
