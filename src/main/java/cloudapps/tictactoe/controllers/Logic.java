50f10f309fdbb9365adbdda6e24b1b460bb3ddf2
david-rojo <davidrojotech@gmail.com>
2020-11-28 22:59:00 +0100

    implemented test in LogicTest that receives a dummy Controller object

---
+++ src/main/java/cloudapps/tictactoe/controllers/Logic.java
@@ -0,0 +1,9 @@
+package cloudapps.tictactoe.controllers;
+
+public class Logic {
+	
+	public Controller getController() {
+		return new StartController(null);
+	}
+
+}
--- src/test/java/cloudapps/tictactoe/controllers/LogicTest.java
+++ src/test/java/cloudapps/tictactoe/controllers/LogicTest.java
@@ -1,12 +1,23 @@
 package cloudapps.tictactoe.controllers;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LogicTest {
 	
+	private Logic logic;
+	
+	@BeforeEach
+	public void setup() {
+		logic = new Logic();
+	}
+	
 	@Test
 	void testGivenNewLogicWhenGetControllerThenController() {
-		
+		assertThat(this.logic.getController(), instanceOf(Controller.class));
 	}
 
 }
