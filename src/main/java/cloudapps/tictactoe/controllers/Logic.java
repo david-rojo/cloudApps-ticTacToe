3bfdfd2aabcbfe1971b033b3938bdf26d510703a
david-rojo <davidrojotech@gmail.com>
2020-11-28 23:22:10 +0100

    refactor Logic constructor and methods to populate needed attributes

--- src/main/java/cloudapps/tictactoe/controllers/Logic.java
+++ src/main/java/cloudapps/tictactoe/controllers/Logic.java
@@ -1,9 +1,29 @@
 package cloudapps.tictactoe.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import cloudapps.tictactoe.models.Game;
+import cloudapps.tictactoe.models.State;
+import cloudapps.tictactoe.models.StateValue;
+
 public class Logic {
 	
-	public Controller getController() {
-		return new StartController(null);
+	private Game game;
+	private State state;
+	private Map<StateValue, Controller> controllers;
+	
+	public Logic() {
+		this.game = new Game();
+		this.state = new State();
+		this.controllers = new HashMap<StateValue, Controller>();
+	    this.controllers.put(StateValue.INITIAL, new StartController(this.game));
+	    this.controllers.put(StateValue.IN_GAME, new PlayController(this.game));
+	    this.controllers.put(StateValue.RESUME, new ResumeController(this.game));
+	    this.controllers.put(StateValue.EXIT, null);
 	}
-
+	
+	public Controller getController() {
+		return this.controllers.get(this.state.getStateValue());
+	}
 }
