2f279967fb2913b3f09a23322887da8728a7b95d
david-rojo <davidrojotech@gmail.com>
2020-11-29 14:27:15 +0100

    added empty test to check that when View.interact then UseCaseController.interact executed

---
+++ src/test/java/cloudapps/tictactoe/views/console/ViewTest.java
@@ -0,0 +1,12 @@
+package cloudapps.tictactoe.views.console;
+
+import org.junit.jupiter.api.Test;
+
+class ViewTest {
+	
+	@Test
+	void testGivenViewWhenInteractThenUseCaseControllerInteract() {
+		
+	}
+
+}
