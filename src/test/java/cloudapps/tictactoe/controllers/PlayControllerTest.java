8dcc38da7b4fffb6beeb4a1046f911680337c1f3
david-rojo <davidrojotech@gmail.com>
2020-11-29 01:51:57 +0100

    implemented test to check if PlayController calls next State but fails, not called

--- src/test/java/cloudapps/tictactoe/controllers/PlayControllerTest.java
+++ src/test/java/cloudapps/tictactoe/controllers/PlayControllerTest.java
@@ -1,11 +1,33 @@
 package cloudapps.tictactoe.controllers;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import cloudapps.tictactoe.models.Game;
+import cloudapps.tictactoe.models.State;
+
+@ExtendWith(MockitoExtension.class)
 class PlayControllerTest {
-
+	
+	@Mock
+    private State state;
+	
+	@Mock
+	private Game game;
+	
+	@InjectMocks
+    private PlayController playController;
+	
 	@Test
 	void testGivenPlayControllerWhenIsTicTacToeThenNextState() {
-    
+		when(this.game.isTicTacToe()).thenReturn(true);
+		this.playController.isTicTacToe();
+	    verify(this.state).next();
 	}
 }
